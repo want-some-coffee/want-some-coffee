45b7a5e26c0108a510aa811dbd7c9adfa9f67230
Amy Ahn <amy.ahn.kr@gmail.com>
2024-07-17 00:00:17 -0400

    index updated

--- app/service.cafe.js
+++ app/service.cafe.js
@@ -1,17 +1,6 @@
-export const yelpApiKey =
-  'cLCckwvPZ64EAo3l2oCqdkYwO92JumAudkV78b84mY_4StYXylEUG0u4R6exM-pnEv3OGRQK16VM_jvgBWBHTwR0jqrBEmhLOWNYTEuaSoodzEYYMAl2PW9ZWBSOZnYx';
-
 export async function getYelpData(map) {
   const location = 'Toronto';
   const category = 'cafes';
-
-  const options = {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${yelpApiKey}`,
-      'Content-Type': 'application/json',
-    },
-  };
 
   try {
     const response = await fetch(
@@ -30,17 +19,29 @@ export async function getYelpData(map) {
   }
 }
 
-// // search for restaurants
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     'Authorization': `Bearer ${apiKey}`,
-//     'Content-Type': 'application/json'
-//   }
-// };
+export function displayResultsOnMap(businesses, map) {
+  businesses.forEach((business) => {
+    const marker = new google.maps.Marker({
+      position: {
+        lat: business.coordinates.latitude,
+        lng: business.coordinates.longitude
+      },
+      map: map,
+      title: business.name
+    });
 
-// fetch('https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20', options)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
-// Test change
+    const infowindow = new google.maps.InfoWindow({
+      content: `
+                <div>
+                    <h3>${business.name}</h3>
+                    <p>Rating: ${business.rating}</p>
+                    <p>${business.location.address1}, ${business.location.city}</p>
+                </div>
+            `
+    });
+
+    marker.addListener('click', () => {
+      infowindow.open(map, marker);
+    });
+  });
+}
