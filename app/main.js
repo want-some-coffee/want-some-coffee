78a6796d195e553778708ab4dec6b488cca98af8
Amy Ahn <amy.ahn.kr@gmail.com>
2024-07-17 12:11:50 -0400

    index-fetch api updated

--- app/main.js
+++ app/main.js
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 import { getCafes } from './cafes.js';
 
 // 윈도우 로딩되면 이벤트 실행
-document.addEventListener('DOMContentLoaded', () => {
-  getCafes();
+document.addEventListener('DOMContentLoaded', async () => {
+  await getCafes();
 });
 
 // 푸터 연도표기
-=======
->>>>>>> fec981de25d1f1b9f13ce37a33942f165bbb2273
 document.getElementById('copyRightYear').textContent = new Date().getFullYear();
-// Test change
