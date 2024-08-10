8bce76235dc5cc1393673c124c48df118425aa1c
Heejin <lalabori@naver.com>
2024-08-10 22:14:39 +0900

    contact-us

--- app/contact-us.js
+++ app/contact-us.js
@@ -19,5 +19,27 @@ btnInsert.onclick = function() {
     }
 }
 
-
+document.addEventListener('DOMContentLoaded', function () {
+    const themeToggleButton = document.getElementById('bd-theme-toggle');
+  
+    function setTheme(theme) {
+      document.documentElement.setAttribute('data-bs-theme', theme);
+      localStorage.setItem('theme', theme);
+      themeToggleButton.classList.toggle('dark-mode', theme === 'dark');
+    }
+  
+    themeToggleButton.addEventListener('click', function () {
+      const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+      if (currentTheme === 'dark') {
+        setTheme('light');
+      } else {
+        setTheme('dark');
+      }
+    });
+  
+    // Load theme from local storage
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    setTheme(savedTheme);
+  });
+  
 
